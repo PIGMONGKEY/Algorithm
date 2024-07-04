0d1b63b11b5823522b67fda0d9c5e83e4a8ba68d
이원희 <pigmong02020202@gmail.com>
2024-07-05 03:52:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.18 ms, Memory: 70.9 MB -BaekjoonHub

--- Java/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ Java/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,20 +3,23 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] numberStrings = s.split(" ");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        StringBuilder sb = new StringBuilder();
         
-        // comparator를 사용하여 정렬
-        Arrays.sort(numberStrings, (o1, o2) -> {
-            return Integer.parseInt(o1) - Integer.parseInt(o2);
-        });
-        
-        // 오름차순으로 정렬된 배열을 돌며 처음과 마지막만 답변에 추가
-        for (int i=0; i<numberStrings.length; i++) {
-            if (i == 0)
-                answer += numberStrings[i] + " ";
-            if (i == numberStrings.length - 1)
-                answer += numberStrings[i];
+        for (String number : s.split(" ")) {
+            int n = Integer.parseInt(number);
+            if (n > max)
+                max = n;
+            if (n < min)
+                min = n;
         }
+        
+        sb.append(min);
+        sb.append(" ");
+        sb.append(max);
+        
+        answer = sb.toString();
         
         return answer;
     }
