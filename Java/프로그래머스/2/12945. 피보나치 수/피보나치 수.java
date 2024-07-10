70362a4b66c40d7ab3cf2eee83fe7027c67feded
이원희 <pigmong02020202@gmail.com>
2024-07-10 13:19:04 +0900

    [level 2] Title: 피보나치 수, Time: 3.05 ms, Memory: 70.4 MB -BaekjoonHub

--- Java/프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ Java/프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -2,15 +2,14 @@ import java.math.*;
 
 class Solution {
     public int solution(int n) {
-        BigInteger[] memo = new BigInteger[n+1];
+        long[] memo = new long[n+1];
         
-        memo[0] = new BigInteger("0");
-        memo[1] = new BigInteger("1");
+        memo[0] = 0L;
+        memo[1] = 1L;
         
-        for (int i=2; i<=n; i++) {
-            memo[i] = memo[i-2].add(memo[i-1]);
-        }
+        for (int i=2; i<=n; i++)
+            memo[i] = (memo[i-2] + memo[i-1]) % 1234567L;
         
-        return memo[n].remainder(new BigInteger("1234567")).intValue();
+        return (int) memo[n];
     }
 }
