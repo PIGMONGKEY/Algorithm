00f3d9da683952b692f0021172f9cf52ddcf1ac6
이원희 <pigmong02020202@gmail.com>
2024-07-23 14:55:32 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- Java/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ Java/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -4,14 +4,12 @@ class Solution {
         int curB = a > b ? a - 1 : b - 1;
         int round = 1;
         
-        while (true) {
-            if (curB - 1 == curA && curA / 2 == curB / 2)
-                break;
+        while (curA != curB) {
             curA /= 2;
             curB /= 2;
             round++;
         }
         
-        return round;
+        return round - 1;
     }
 }
