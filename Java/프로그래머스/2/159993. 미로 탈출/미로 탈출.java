7a43687c7723b70fb174ce67e641df41c4a04342
이원희 <pigmong02020202@gmail.com>
2024-08-18 23:50:09 +0900

    [level 2] Title: 미로 탈출, Time: 16.83 ms, Memory: 86.1 MB -BaekjoonHub

--- Java/프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ Java/프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,25 +1,22 @@
 import java.util.*;
+import java.lang.Thread;
 
 class Solution {
     
+    private static char[][] maze;
+    private static final int[] increaseValue = {1, 0, -1, 0};
+    private static int toLTime;
+    private static int toETime;
+    
     public int solution(String[] maps) {
-        int lever = 0;
-        int end = 0;
-        int time = 0;
         int height = maps.length;
         int width = maps[0].length();
         char c;
         
-        int[] cur = {};
-        int[] next = {};
-        int[] increaseValue = {1, 0, -1, 0};
-        int[][] places = new int[3][3];
-        char[][] maze = new char[height][width];
-        boolean[][] isVisited = new boolean[height][width];
+        int[][] places = new int[3][2];
+        maze = new char[height][width];
         
-        Queue<int[]> queue = new LinkedList<>();
-        
-        // char 2차원 배열에 저장
+        // 시작위치, 레버위치, 출구위치 찾으면서 지도 그리기
         for (int i=0; i<height; i++) {
             for (int j=0; j<width; j++) {
                 c = maps[i].charAt(j);
@@ -29,101 +26,60 @@ class Solution {
                     case 'S':
                         places[0][0] = i;
                         places[0][1] = j;
-                        places[0][2] = 0;
                         break;
                     case 'L':
                         places[1][0] = i;
                         places[1][1] = j;
-                        places[1][2] = 0;
                         break;
                     case 'E':
                         places[2][0] = i;
                         places[2][1] = j;
-                        places[2][2] = 0;
                         break;
                 }
             }
         }
         
-        // 레버까지 길 찾기
-        queue.add(places[0]);
+        toLTime = findShortestPath(places[0], places[1]);
+        toETime = findShortestPath(places[1], places[2]);
+        
+        return toLTime == -1 || toETime == -1 ? -1 : toLTime + toETime;
+    }
+    
+    private int findShortestPath(int[] start, int[] dest) {
+        int height = maze.length;
+        int width = maze[0].length;
+        
+        int[] cur = {start[0], start[1], 0};
+        int[] next = {};
+        boolean[][] isVisited = new boolean[height][width];
+        Queue<int[]> queue = new LinkedList<>();
+        
+        queue.add(cur);
         
         while (!queue.isEmpty()) {
             cur = queue.poll();
             
-            // 미로 범위를 벗어난 경우 continue
             if (cur[0] >= height || cur[0] < 0 || cur[1] >= width || cur[1] < 0)
                 continue;
-            
-            // 벽인 경우 continue
             if (maze[cur[0]][cur[1]] == 'X')
                 continue;
-            
-            // 이미 방문한 곳인 경우 continue
             if (isVisited[cur[0]][cur[1]])
                 continue;
-            else
-                isVisited[cur[0]][cur[1]] = true;
             
-            if (cur[0] == places[1][0] && cur[1] == places[1][1]) {
-                lever += cur[2];
-                end = lever;
-                break;
-            }
+            if (cur[0] == dest[0] && cur[1] == dest[1])
+                return cur[2];
             
-            for (int i=0; i<increaseValue.length; i++) {
+            isVisited[cur[0]][cur[1]] = true;
+            
+            for (int i=0; i<4; i++) {
                 next = new int[3];
                 next[0] = cur[0] + increaseValue[i];
-                next[1] = cur[1] + increaseValue[(i + 1) % increaseValue.length];
+                next[1] = cur[1] + increaseValue[(i + 1) % 4];
                 next[2] = cur[2] + 1;
                 queue.add(next);
             }
         }
         
-        queue.clear();
-        
-        for (int i=0; i<height; i++)
-            Arrays.fill(isVisited[i], false);
-        
-        // 레버에서 출구까지 길 찾기
-        queue.add(places[1]);
-        
-        while (!queue.isEmpty()) {
-            cur = queue.poll();
-            
-            // 미로 범위를 벗어난 경우 continue
-            if (cur[0] >= height || cur[0] < 0 || cur[1] >= width || cur[1] < 0)
-                continue;
-            
-            // 벽인 경우 continue
-            if (maze[cur[0]][cur[1]] == 'X')
-                continue;
-            
-            // 이미 방문한 곳인 경우 continue
-            if (isVisited[cur[0]][cur[1]])
-                continue;
-            else
-                isVisited[cur[0]][cur[1]] = true;
-            
-            if (cur[0] == places[2][0] && cur[1] == places[2][1]) {
-                end += cur[2];
-                break;
-            }
-            
-            for (int i=0; i<increaseValue.length; i++) {
-                next = new int[3];
-                next[0] = cur[0] + increaseValue[i];
-                next[1] = cur[1] + increaseValue[(i + 1) % increaseValue.length];
-                next[2] = cur[2] + 1;
-                queue.add(next);
-            }
-        }
-        
-        queue.clear();
-        
-        if (lever == 0 || lever == end || end == 0)
-            return -1;
-        else
-            return end;
+        return -1;
     }
 }
