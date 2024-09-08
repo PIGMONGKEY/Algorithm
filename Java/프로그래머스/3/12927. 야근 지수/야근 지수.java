7806a6c2ceeff3e1c61f5ad071920a359f6b907c
이원희 <pigmong02020202@gmail.com>
2024-09-08 20:58:57 +0900

    [level 3] Title: 야근 지수, Time: 143.93 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        int temp = 0;
+        long answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        
+        for (int work : works)
+            pq.add(work);
+        
+        temp = pq.poll();
+        
+        while (n > 0) {            
+            if (temp >= pq.peek()) {
+                temp--;
+                n--;
+            } else {
+                pq.add(temp);
+                temp = pq.poll();
+            }
+        }
+        
+        pq.add(temp);
+        
+        for (Integer work : pq) {
+            if (work <= 0)
+                continue;
+            answer += ((long) work * (long) work);
+        }
+        
+        return answer;
+    }
+}
