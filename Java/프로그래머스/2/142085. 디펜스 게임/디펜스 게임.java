e71414cb8fb68d076471ab3a2fb02288732bb8a0
이원희 <pigmong02020202@gmail.com>
2024-09-11 13:36:08 +0900

    [level 2] Title: 디펜스 게임, Time: 94.61 ms, Memory: 128 MB -BaekjoonHub

--- Java/프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ Java/프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -5,29 +5,19 @@ class Solution {
         int round = 1;
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         
-        // 무적권 안 쓰고 갈 수 있을 때까지 전진
-        while (round < enemy.length + 1 && n >= enemy[round - 1]) {
-            n -= enemy[round - 1];
-            pq.add(enemy[round - 1]);
-            round++;
-        }
-        
-        // 현제 라운드를 포함한 라운드 중 적 수가 가장 많은 라운드에 무적권 사용
-        while (true) {
-            if (round > enemy.length || (n < enemy[round - 1] && k <= 0))
-                break;
+        for (int e : enemy) {
+            pq.add(e);
             
-            if (n >= enemy[round - 1]) {
-                n -= enemy[round - 1];
-                pq.add(enemy[round - 1]);
+            if (n >= e) {
+                n -= e;
                 round++;
             } else if (k > 0) {
-                pq.add(enemy[round - 1]);
                 n += pq.poll();
-                n -= enemy[round - 1];
+                n -= e;
                 round++;
                 k--;
-            }
+            } else
+                break;
         }
         
         return round - 1;
