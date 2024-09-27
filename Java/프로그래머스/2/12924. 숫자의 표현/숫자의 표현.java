0d22e946adeae0b3a25f4c55dc4d8c68a4fffbb7
이원희 <pigmong02020202@gmail.com>
2024-09-27 21:45:29 +0900

    [level 2] Title: 숫자의 표현, Time: 0.43 ms, Memory: 51.8 MB -BaekjoonHub

--- Java/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ Java/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,23 +1,16 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0, left = 0, right = 0, sum = 1;
-        int[] arr = new int[n];
-        
-        for (int i=0; i<n; i++)
-            arr[i] = i+1;
+        int answer = 0, left = 1, right = 1, sum = 1;
         
         while (true) {
-            if (sum == n) {
-                answer++;
-                if (++right >= n)
+            if (sum <= n) {
+                if (sum == n)
+                    answer++;
+                if (++right > n)
                     break;
-                sum += arr[right];
-            } else if (sum < n) {
-                if (++right >= n)
-                    break;
-                sum += arr[right];
+                sum += right;
             } else {
-                sum -= arr[left++];
+                sum -= left++;
             }
         }
         
